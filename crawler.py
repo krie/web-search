55ecd113c67f6720d49691ba536a1024e8d30654
s0544363 <s0544363@htw-berlin.de>
2015-10-28 12:16:14 +0100

    Adjazenzsmatrix in Crawler implementiert

--- crawler.py
+++ crawler.py
@@ -1,6 +1,9 @@
-from bs4 import BeautifulSoup
-import requests
 from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+import numpy as np
+import requests
+import collections as col
 
 __author__ = 'Alexander'
 
@@ -9,22 +12,36 @@ class crawler():
         self.seedfile = seedfile
         self.urlSetDone = []     # Bereits besuchte URLs
         self.urlset = []
+        self.uebergangsListe = col.defaultdict(dict)
     def crawl(self):
         with open("seed.dat", "r") as fileInput:
-            for zeile in fileInput:
-                self.urlset.append(zeile)
-        while(len(self.urlset) > 0):
+            alist = fileInput.read().splitlines()
+            for line in alist:
+                self.urlset.append(line)
+        while(self.urlset):
             url = self.urlset.pop()
-            print('Aktuelle URL: {}, noch {} im URLset'.format(url, len(self.urlset)))
+            #print('Aktuelle URL: {}, noch {} im URLset'.format(url, len(self.urlset)))
             self.urlSetDone.append(url)
             r = requests.get(url)
             data = r.text
             soup = BeautifulSoup(data)
+            self.uebergangsListe[url][url] = 0
             for link in soup.findAll('a'):
-                print('Link zu {} in url: {} gefunden'.format(link.get('href'), url))
+                #print('Link zu {} in url: {} gefunden'.format(link.get('href'), url))
                 newUrl = urljoin(url, link.get('href'))
+                self.uebergangsListe[url][newUrl] = 1
                 if newUrl not in self.urlSetDone:
                     self.urlset.append(newUrl)
-
+    def getUebergangsMatrix(self):
+        matrix = np.zeros(shape=(len(self.urlSetDone), len(self.urlSetDone)));
+        geordneteListe = col.OrderedDict(sorted(self.uebergangsListe.items()))
+        #print(geordneteListe)
+        for seite, links in geordneteListe.items():
+            seiteIndex = list(geordneteListe.keys()).index(seite)
+            for link, value in links.items():
+                linkIndex = list(geordneteListe.keys()).index(link)
+                matrix[seiteIndex, linkIndex] = value
+        print(matrix)
 mycrawler = crawler("seed.dat")
 mycrawler.crawl()
+mycrawler.getUebergangsMatrix()
