ad822f10a4167d91c63bb83a3d1203fb52da6207
Julian Krieft <s0548838@htw-berlin.de>
2015-11-04 12:05:19 +0100

    PageRank

--- pageRank.py
+++ pageRank.py
@@ -37,13 +37,110 @@ class pageRank:
         pi = []
         for i in self.websiteMatrix:
             pi.append(1/len(self.websiteMatrix))
-
-        delta = 0
-        while delta > 0.04:
-            pi = pi * self.websiteMatrix
-
-
-
         print(pi)
-        print(self.websiteMatrix)
+        delta = 1
+        while delta > 0.04:
+            piOld = pi.copy()
 
+            for i in range(0, len(pi)):
+                sum = 0
+                for j in range(0, len(pi)):
+                    sum += piOld[j] * self.websiteMatrix[j][i]
+                pi[i] = sum
+            """
+            i = 0
+            pi[i] = piOld[0] * self.websiteMatrix[0][i]\
+                    + piOld[1] * self.websiteMatrix[1][i]\
+                    + piOld[2] * self.websiteMatrix[2][i]\
+                    + piOld[3] * self.websiteMatrix[3][i]\
+                    + piOld[4] * self.websiteMatrix[4][i]\
+                    + piOld[5] * self.websiteMatrix[5][i]\
+                    + piOld[6] * self.websiteMatrix[6][i]\
+                    + piOld[7] * self.websiteMatrix[7][i]
+
+            i = 1
+            pi[i] = piOld[0] * self.websiteMatrix[0][i]\
+                    + piOld[1] * self.websiteMatrix[1][i]\
+                    + piOld[2] * self.websiteMatrix[2][i]\
+                    + piOld[3] * self.websiteMatrix[3][i]\
+                    + piOld[4] * self.websiteMatrix[4][i]\
+                    + piOld[5] * self.websiteMatrix[5][i]\
+                    + piOld[6] * self.websiteMatrix[6][i]\
+                    + piOld[7] * self.websiteMatrix[7][i]
+
+            i = 2
+            pi[i] = piOld[0] * self.websiteMatrix[0][i]\
+                    + piOld[1] * self.websiteMatrix[1][i]\
+                    + piOld[2] * self.websiteMatrix[2][i]\
+                    + piOld[3] * self.websiteMatrix[3][i]\
+                    + piOld[4] * self.websiteMatrix[4][i]\
+                    + piOld[5] * self.websiteMatrix[5][i]\
+                    + piOld[6] * self.websiteMatrix[6][i]\
+                    + piOld[7] * self.websiteMatrix[7][i]
+
+            i = 3
+            pi[i] = piOld[0] * self.websiteMatrix[0][i]\
+                    + piOld[1] * self.websiteMatrix[1][i]\
+                    + piOld[2] * self.websiteMatrix[2][i]\
+                    + piOld[3] * self.websiteMatrix[3][i]\
+                    + piOld[4] * self.websiteMatrix[4][i]\
+                    + piOld[5] * self.websiteMatrix[5][i]\
+                    + piOld[6] * self.websiteMatrix[6][i]\
+                    + piOld[7] * self.websiteMatrix[7][i]
+
+            i = 4
+            pi[i] = piOld[0] * self.websiteMatrix[0][i]\
+                    + piOld[1] * self.websiteMatrix[1][i]\
+                    + piOld[2] * self.websiteMatrix[2][i]\
+                    + piOld[3] * self.websiteMatrix[3][i]\
+                    + piOld[4] * self.websiteMatrix[4][i]\
+                    + piOld[5] * self.websiteMatrix[5][i]\
+                    + piOld[6] * self.websiteMatrix[6][i]\
+                    + piOld[7] * self.websiteMatrix[7][i]
+
+            i = 5
+            pi[i] = piOld[0] * self.websiteMatrix[0][i]\
+                    + piOld[1] * self.websiteMatrix[1][i]\
+                    + piOld[2] * self.websiteMatrix[2][i]\
+                    + piOld[3] * self.websiteMatrix[3][i]\
+                    + piOld[4] * self.websiteMatrix[4][i]\
+                    + piOld[5] * self.websiteMatrix[5][i]\
+                    + piOld[6] * self.websiteMatrix[6][i]\
+                    + piOld[7] * self.websiteMatrix[7][i]
+
+            i = 6
+            pi[i] = piOld[0] * self.websiteMatrix[0][i]\
+                    + piOld[1] * self.websiteMatrix[1][i]\
+                    + piOld[2] * self.websiteMatrix[2][i]\
+                    + piOld[3] * self.websiteMatrix[3][i]\
+                    + piOld[4] * self.websiteMatrix[4][i]\
+                    + piOld[5] * self.websiteMatrix[5][i]\
+                    + piOld[6] * self.websiteMatrix[6][i]\
+                    + piOld[7] * self.websiteMatrix[7][i]
+
+            i = 7
+            pi[i] = piOld[0] * self.websiteMatrix[0][i]\
+                    + piOld[1] * self.websiteMatrix[1][i]\
+                    + piOld[2] * self.websiteMatrix[2][i]\
+                    + piOld[3] * self.websiteMatrix[3][i]\
+                    + piOld[4] * self.websiteMatrix[4][i]\
+                    + piOld[5] * self.websiteMatrix[5][i]\
+                    + piOld[6] * self.websiteMatrix[6][i]\
+                    + piOld[7] * self.websiteMatrix[7][i]
+
+            delta = abs(pi[0] - piOld[0])\
+                    + abs(pi[1] - piOld[1])\
+                    + abs(pi[2] - piOld[2])\
+                    + abs(pi[3] - piOld[3])\
+                    + abs(pi[4] - piOld[4])\
+                    + abs(pi[5] - piOld[5])\
+                    + abs(pi[6] - piOld[6])\
+                    + abs(pi[7] - piOld[7])\
+            """
+            delta = 0
+            for k in range(0, len(pi)):
+                delta += abs(pi[k] - piOld[k])
+
+
+        print(self.websiteMatrix)
+        print(pi)
