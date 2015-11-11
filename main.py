e9c7b1f6ee2b7e255371c097a3f027a972d8d3b3
Julian Krieft <s0548838@htw-berlin.de>
2015-11-11 11:21:25 +0100

    scoring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pageRank import pageRank
 from crawler import crawler
 from indexing import indexer
+from scoring import scoring
 
 
 testMatrix = [[0, 1, 1, 1, 0, 0, 0, 0],
@@ -14,13 +15,15 @@ testMatrix = [[0, 1, 1, 1, 0, 0, 0, 0],
 
 mycrawler = crawler("seed.dat")
 mycrawler.crawl()
+
 myMatrix = mycrawler.getUebergangsMatrix()
 myRank = pageRank(websiteMatrix=myMatrix)
 myRank.calcRank()
+
 myIndexer = indexer(mycrawler.urlSetDone)
 myIndexer.runIndexing()
 myIndexer.outputIndexToFile()
-index = myIndexer.indexDict
 
-for wort, anzahl in index["and"].items():
-    print("{} {}".format(wort, anzahl))
+index = myIndexer.indexDict
+myScoring = scoring(len(mycrawler.urlSetDone), index)
+myScoring.calcScoreForQuery("and")
--- scoring.py
+++ scoring.py
@@ -1,1 +1,42 @@
 __author__ = 'julian'
+import math
+class scoring:
+    def __init__(self, n, indexList):
+        self.n = n
+        self.indexList = indexList
+        self.weigthMatrix = indexList
+        self.docLength = [0] * n
+        self.calcWeightMatrix()
+        self.calcDocLength()
+
+    def termFreq(self,t, d):
+        return self.indexList[t][d]
+
+    def docFreq(self, t):
+        return len(self.indexList[t])
+
+    def weight(self, t, d):
+        return (1 + math.log10(self.termFreq(t, d))) * math.log10(self.n/self.docFreq(t))
+
+
+    def calcWeightMatrix(self):
+        for wort, docs in self.indexList.items():
+            for doc, freq in docs.items():
+                self.weigthMatrix[wort][doc] = self.weight(wort, doc)
+
+    def calcDocLength(self):
+        for wort, docs in self.weigthMatrix.items():
+            for doc, weight in docs.items():
+                self.docLength[doc] = self.docLength[doc] + weight * weight
+
+
+    def calcScoreForQuery(self, query):
+        score = [0] * self.n
+        terms = query.split(" ")
+        for term in terms:
+            for doc, weight in self.weigthMatrix[term].items():
+                score[doc] = weight * 1
+        print(score)
+
+
+
